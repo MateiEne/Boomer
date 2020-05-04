55a93d8d518deb6b37a09506ff53a316ca28fe2a
EneMatei <ene.matei21@gmail.com>
2020-05-04 22:45:45 +0300

    create surprise class

---
+++ Surprise.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <iostream>
+
+#include "Constants.h"
+#include "MatPos.h"
+#include "Animation.h"
+#include "World.h"
+#include "MathUtils.h" 
+
+using namespace std;
+
+class Surprise
+{
+};
+
